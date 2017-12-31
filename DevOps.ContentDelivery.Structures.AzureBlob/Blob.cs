6c5ec9c68f6d52eefc20663f5d1a41ae84e796b8
Christopher Dorst <chris@dorst.email>
2017-12-30 21:25:11 -0600

    Add Blob

---
+++ DevOps.ContentDelivery.Structures.AzureBlob/Blob.cs
@@ -0,0 +1,14 @@
+﻿namespace DevOps.ContentDelivery.Structures.AzureBlob
+{
+    public struct Blob
+    {
+        public Blob(string container, string name)
+        {
+            Container = container;
+            Name = name;
+        }
+
+        public string Container { get; set; }
+        public string Name { get; set; }
+    }
+}
